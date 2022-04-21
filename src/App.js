0e54bad52f0f8d6fa64116e92039f6e0a3d67048
Julien Fleury <julienfleury93@gmail.com>
2022-04-21 18:05:02 +0200

    Added navigation elements for mainNavigation and added some styling to the homepage.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/navigation/navigation.component';
+// Routes
+import Homepage from './pages/homePage/homePage.page';
+import AboutPage from './pages/about/about.page';
+import ContactPage from './pages/contact/contact.page';
+import PortfolioPage from './pages/portfolio/portfolio.page';
+import SpecificPortfolioPage from './pages/specificPortfolio/specificPortfolio.page';
 
 const App = () => {
   return (
     <div className="App">
-      
+      <Navigation />
+      <Routes>
+        <Route exact path='/' element={<Homepage />} />
+        <Route exact path='/about' element={<AboutPage />} />
+        <Route exact path='/contact' element={<ContactPage />} />
+        <Route exact path='/portfolio' element={<PortfolioPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import './navigation.styles.scss';
+import { Link, useLocation } from "react-router-dom";
+import { ReactComponent as FleuryLogo } from '../../assets/fpLogo.svg';
 
 const Navigation = () => {
+    const location = useLocation().pathname;
     return (
         <nav className="navigationContainer">
-
-        </nav>
+            {/* Logo and LangSwitch Container */}
+            <div className="logoContainer me-auto">
+                <Link to={'/'}>
+                    <FleuryLogo />
+                </Link>
+            </div>
+            <div className="mainNavigation d-none d-md-flex">
+                <Link className="mainNavItem" to={'/portfolio'}>PORTFOLIO</Link>
+                <Link className="mainNavItem" to={'/about'}>ABOUT</Link>
+                <Link className="mainNavItem" to={'/contact'}>CONTACT</Link>
+            </div>
+        </nav >
     );
 }
 
