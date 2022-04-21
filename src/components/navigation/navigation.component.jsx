1ad4c8c937a79f06ef6706a48726aa6ec17f23f2
Julien Fleury <julienfleury93@gmail.com>
2022-04-21 17:42:55 +0200

    Added navigation component and logo svg.

---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return (
+        <nav className="navigationContainer">
+
+        </nav>
+    );
+}
+
+export default Navigation;
