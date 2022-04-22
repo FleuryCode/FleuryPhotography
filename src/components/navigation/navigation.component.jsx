cb06b4638ba73e9596cce7d129272b69ca04a875
Julien Fleury <julienfleury93@gmail.com>
2022-04-22 16:28:37 +0200

    Nav functionality all good.

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import './navigation.styles.scss';
 import { Link, useLocation } from "react-router-dom";
 import { ReactComponent as FleuryLogo } from '../../assets/fpLogo.svg';
 
 const Navigation = () => {
     const location = useLocation().pathname;
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const mobileMenuClick = () => {
+        setMenuOpen(!menuOpen);
+    };
     return (
         <nav className="navigationContainer">
             {/* Logo and LangSwitch Container */}
@@ -17,6 +22,21 @@ const Navigation = () => {
                 <Link className="mainNavItem" to={'/portfolio'}>PORTFOLIO</Link>
                 <Link className="mainNavItem" to={'/about'}>ABOUT</Link>
                 <Link className="mainNavItem" to={'/contact'}>CONTACT</Link>
+            </div>
+            <div className="mobileNavigation d-flex d-md-none">
+                <div className="burgerMenuContainer">
+                    <div onClick={mobileMenuClick} className={`${menuOpen ? 'open' : ''} burgerMenu`}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <div className={`${menuOpen ? 'd-flex' : 'd-none'} mobileNavLinks`}>
+                    <Link className="mobileNavItem" to={'/portfolio'}>PORTFOLIO</Link>
+                    <Link className="mobileNavItem" to={'/about'}>ABOUT</Link>
+                    <Link className="mobileNavItem" to={'/contact'}>CONTACT</Link>
+                </div>
             </div>
         </nav >
     );
