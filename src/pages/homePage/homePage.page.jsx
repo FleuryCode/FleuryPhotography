a4825db65fc0fd887c62d590f521730ded6f19f1
Julien Fleury <julien@fleuryphotography.com>
2022-04-23 21:44:07 +0200

    Getting closer, but ugh.

--- src/pages/homePage/homePage.page.jsx
+++ src/pages/homePage/homePage.page.jsx
@@ -7,22 +7,43 @@ import ProductImage from '../../assets/heroImages/elianabowl_main.jpg';
 import VideoImage from '../../assets/heroImages/beersplash_main.jpg';
 
 const Homepage = () => {
-    const [imageIndex, setImageIndex] = useState(0);
-    const [endAnim, setEndAnim] = useState(true);
-
-    const [test, setTest] = useState(true);
-    useEffect(() => {
-        console.log('Lets hope this works.');
-    }, [test]);
+    let imageIndex = 0;
+    let endAnim = true;
+    const testFunc = () => {
+        // const newElement = document.querySelector(`.hero${imageIndex + 1}`);
+        // const oldElement = document.querySelector(`.hero${oldImageIndex}`);
+        // newElement.classList.add('active');
+        // oldElement.classList.remove('active');
+    };
 
     window.addEventListener('mousewheel', (e) => {
         const yDelta = e.wheelDeltaY;
-        if (yDelta / 120 > 0) {
+        if (endAnim) {
+            endAnim = false;
             setTimeout(() => {
-                setTest(!test);
+                endAnim = true;
+                console.log('Can Scroll');
             }, 2000);
+            if (yDelta / 120 > 0) {
+                if (imageIndex === 0) {
+                    imageIndex = 3;
+                } else {
+                    imageIndex--;
+                }
+                console.log(imageIndex);
+            } else {
+                if (imageIndex === 3) {
+                    imageIndex = 0;
+                } else {
+                    imageIndex++;
+                }
+                console.log(imageIndex);
+            }
+            testFunc();
         }
     });
+
+   
 
     // Image Changing
     // window.addEventListener('mousewheel', (e) => {
@@ -61,16 +82,17 @@ const Homepage = () => {
     return (
         <div className="homepageContainer">
             <div style={{ transform: `translateY(-${imageIndex * 100}%)` }} className="innerContainer">
-                <div className="homeFoodContainer">
+                <div className={`homeFoodContainer hero1 active`}>
                     <h1>Food</h1>
+                    <button onClick={testFunc}>Test</button>
                 </div>
-                <div className="homeBeverageContainer">
+                <div className="homeBeverageContainer hero2">
                     <h1>Drink</h1>
                 </div>
-                <div className="homeProductContainer">
+                <div className="homeProductContainer hero3">
                     <h1>Product</h1>
                 </div>
-                <div className="homeVideoContainer">
+                <div className="homeVideoContainer hero4">
                     <h1>Video</h1>
                 </div>
             </div>
