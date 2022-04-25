d15face7ba97f6b3d2bcfc498b4143a3bf44a5b9
Julien Fleury <julienfleury93@gmail.com>
2022-04-25 13:46:07 +0200

    Worked out the problem. Need to hook everything up now.

--- src/pages/homePage/homePage.page.jsx
+++ src/pages/homePage/homePage.page.jsx
@@ -9,82 +9,64 @@ import VideoImage from '../../assets/heroImages/beersplash_main.jpg';
 const Homepage = () => {
     let imageIndex = 0;
     let endAnim = true;
-    const testFunc = () => {
-        // const newElement = document.querySelector(`.hero${imageIndex + 1}`);
-        // const oldElement = document.querySelector(`.hero${oldImageIndex}`);
-        // newElement.classList.add('active');
-        // oldElement.classList.remove('active');
-    };
-
     window.addEventListener('mousewheel', (e) => {
         const yDelta = e.wheelDeltaY;
         if (endAnim) {
-            endAnim = false;
-            setTimeout(() => {
-                endAnim = true;
-                console.log('Can Scroll');
-            }, 2000);
             if (yDelta / 120 > 0) {
                 if (imageIndex === 0) {
+                    console.log('Scroll Up');
+                    endAnim = false;
                     imageIndex = 3;
+                    const addHeroElement = document.querySelector(`.hero${imageIndex + 1}`);
+                    const removeHeroElement = document.querySelector(`.hero1`);
+                    addHeroElement.classList.add('active');
+                    removeHeroElement.classList.remove('active');
+                    setTimeout(() => {
+                        console.log('Can Scroll')
+                        endAnim = true;
+                    }, 600);
                 } else {
+                    endAnim = false;
                     imageIndex--;
+                    const addHeroElement = document.querySelector(`.hero${imageIndex + 1}`);
+                    const removeHeroElement = document.querySelector(`.hero${imageIndex + 2}`);
+                    addHeroElement.classList.add('active');
+                    removeHeroElement.classList.remove('active');
+                    setTimeout(() => {
+                        endAnim = true;
+                    }, 600);
                 }
-                console.log(imageIndex);
             } else {
                 if (imageIndex === 3) {
+                    endAnim = false;
                     imageIndex = 0;
+                    const addHeroElement = document.querySelector(`.hero${imageIndex + 1}`);
+                    const removeHeroElement = document.querySelector(`.hero4`);
+                    addHeroElement.classList.add('active');
+                    removeHeroElement.classList.remove('active');
+                    setTimeout(() => {
+                        endAnim = true;
+                    }, 600);
                 } else {
+                    endAnim = false;
                     imageIndex++;
+                    const addHeroElement = document.querySelector(`.hero${imageIndex + 1}`);
+                    const removeHeroElement = document.querySelector(`.hero${imageIndex}`);
+                    addHeroElement.classList.add('active');
+                    removeHeroElement.classList.remove('active');
+                    setTimeout(() => {
+                        endAnim = true;
+                    }, 600);
                 }
-                console.log(imageIndex);
             }
-            testFunc();
         }
     });
-
-   
-
-    // Image Changing
-    // window.addEventListener('mousewheel', (e) => {
-    //     const yDelta = e.wheelDeltaY;
-    //     if(endAnim) {
-    //         setEndAnim(false);
-    //         if(yDelta / 120 > 0) {
-    //             if(imageIndex === 0) {
-    //                 setImageIndex(3);
-    //                 setTimeout(() => {
-    //                     setEndAnim(true);
-    //                 }, 800);
-    //             }else {
-    //                 setImageIndex(imageIndex - 1);
-    //                 setTimeout(() => {
-    //                     setEndAnim(true);
-    //                 }, 800);
-    //             }
-    //         }else {
-    //             if(imageIndex === 3) {
-    //                 setImageIndex(0);
-    //                 setTimeout(() => {
-    //                     setEndAnim(true);
-    //                 }, 800);
-    //             }else {
-    //                 setImageIndex(imageIndex + 1);
-    //                 setTimeout(() => {
-    //                     setEndAnim(true);
-    //                 }, 800);
-    //             }
-    //         }
-    //     }
-    // });
-
 
     return (
         <div className="homepageContainer">
             <div style={{ transform: `translateY(-${imageIndex * 100}%)` }} className="innerContainer">
                 <div className={`homeFoodContainer hero1 active`}>
                     <h1>Food</h1>
-                    <button onClick={testFunc}>Test</button>
                 </div>
                 <div className="homeBeverageContainer hero2">
                     <h1>Drink</h1>
