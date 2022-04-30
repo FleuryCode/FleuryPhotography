4b3ce874c28780e712174d2609dc1984166d3ead
Julien Fleury <julienfleury93@gmail.com>
2022-04-30 20:31:13 +0200

    Made the 404 page.

--- src/pages/notFound/notFound.page.jsx
+++ src/pages/notFound/notFound.page.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import './notFound.styles.scss';
+import ErrorImage from '../../assets/errorImage.png';
 
 const NotFound = () => {
     return (
-        <div className="notFoundContainer">
-            <h1>Not Found 404</h1>
+        <div className="notFoundContainer container-fluid">
+            <div className="row">
+                <div className="col-12">
+                    <h1 className="text-center">Error 404</h1>
+                </div>
+                <div className="col-12">
+                    <h4 className="text-center">Page Not Found</h4>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <div className="errorImageContainer">
+                        <img src={ErrorImage} alt="Broken camera 404 image" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
