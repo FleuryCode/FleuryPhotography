7888f2ffb15b535d0ab2fa56a90cdf55a94e01d2
Julien Fleury <julienfleury93@gmail.com>
2022-08-07 16:44:00 +0200

    feat: added Google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,6 +5,21 @@ import '../styles/globals.scss';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload" id="googleAnalytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
